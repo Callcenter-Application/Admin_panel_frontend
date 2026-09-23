f361ece5681050f49060045f79b308bb634a4dc9
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-09-23 07:26:26 -0500

    feat: feat: initialize project with Vite and React setup

---
+++ src/components/header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => 
+    <header>
+        <h1>Hello world this is the header</h1>
+        <a href="./src/views/login.html" target="_blank" rel="noopener noreferrer">login</a>
+    </header>;
