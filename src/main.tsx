03e48e3648f97bbe0ed7de8a87cb9ea88510e06b
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-09-23 08:25:57 -0500

    feat: implement login page and routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.tsx'
+import { RouterProvider } from 'react-router'; 
 
+
+// 2. Render using the RouterProvider
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={App} />
+  </StrictMode>
+);
---
+++ src/pages/login.tsx
@@ -0,0 +1,4 @@
+export const Login = () => <></>;
+//This has to have all the data in login.html 
+//Delete the /src/pages/login.html and add in here all the information, functions, scripts and tags in here so the router start by showing a login.
+//Do not modify any aspect of the css, style, tag or script of the original login.
